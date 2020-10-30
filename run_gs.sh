9026e2bd42bbfcc9b209bd4c994c7a3d8038cb1b
vishalsunder <sundervishal26@gmail.com>
2020-10-30 17:30:11 -0400

    Update run_gs.sh

--- run_gs.sh
+++ run_gs.sh
@@ -3,17 +3,17 @@ do
     for j in 0.00 0.15 0.25 0.5 0.75 1.00
     do
         python -u main.py \
-            --word-vector "/homes/3/sunder.9/vp-rnn-local/glove/vp_new/glove.42B.300d.txt.pt" \
-            --label-data "/homes/3/sunder.9/vp-rnn-local/data/18/vp.full.labels.json" \
-            --traindev-data "/homes/3/sunder.9/vp-rnn-local/data/18/vp.full.train.json" \
-            --dictionary "/homes/3/sunder.9/vp-rnn-local/data/18/vp.full.dict.json" \
+            --word-vector "glove/vp18.glove.42B.300d.txt.pt" \
+            --label-data "data/vp_18/vp.full.labels.json" \
+            --traindev-data "data/vp_18/vp.full.train.json" \
+            --dictionary "data/vp_18/vp.full.dict.json" \
             --test-data "" \
-            --valid-data "/homes/3/sunder.9/vp-rnn-local/data/18/vp.full.valid.json" \
+            --valid-data "data/vp_18/vp.full.valid.json" \
             --attention-hops 2 \
-            --epochs 4 \
-            --stage2 2 \
+            --epochs 12 \
+            --stage2 3 \
             --nclasses 374 \
-            --batch-size 64 \
+            --batch-size 512 \
             --test-bsize 32 \
             --penalization-coeff 0.0 \
             --margin-pos 0.8 \
@@ -22,7 +22,7 @@ do
             --beta-min 0.5 \
             --lr 0.00004 \
             --num-pos 50000 \
-            --validation-log "logs/vp_new_bert.log" \
+            --validation-log "logs/vp_new_rnn.log" \
             --num-keys 3000 \
             --lamb $i \
             --ploss_wt $j \
